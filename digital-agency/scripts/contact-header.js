655d9e5aed58385b694f862f658071be482b33c2
pgm-behlsava <behlsava@student.arteveldehs.be>
2023-11-01 11:30:07 +0100

    first commit

---
+++ digital-agency/scripts/contact-header.js
@@ -0,0 +1,23 @@
+const header = document.getElementsByClassName("header")[0];
+const wolfIcon = document.querySelector("#wolf-icon");
+const scrollTrigger = 0;
+
+window.onscroll = function () {
+  if (window.scrollY > scrollTrigger || window.pageYOffset > scrollTrigger) {
+    header.classList.add("full-opacity");
+  } else {
+    header.classList.remove("full-opacity");
+  }
+};
+
+wolfIcon.addEventListener("mouseover", () => {
+  wolfIcon.getAttribute("src") === "images/icons8-wolf-black-48.png"
+    ? wolfIcon.setAttribute("src", "images/icons8-wolf-white-48.png")
+    : wolfIcon.setAttribute("src", "images/icons8-wolf-black-48.png");
+});
+
+wolfIcon.addEventListener("mouseout", () => {
+  wolfIcon.getAttribute("src") === "images/icons8-wolf-white-48.png"
+    ? wolfIcon.setAttribute("src", "images/icons8-wolf-black-48.png")
+    : wolfIcon.setAttribute("src", "images/icons8-wolf-white-48.png");
+});
